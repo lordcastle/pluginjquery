73d1071b822f552488ec9d81755bee7b9107e68d
Marco Castillo Gutierrez <tonny1881@gmail.com>
2022-01-02 23:13:56 -0600

    Update and rename tonnyvalidations_config.js to focusValidations_config.js

--- js/focusValidations_config.js
+++ js/focusValidations_config.js
@@ -3,24 +3,24 @@ jQuery(document).ready(function() {
     $("#validar").click(function() {
 
         $(".message_error").remove();
-        $("#nombre").tonnyvalidations({ 
+        $("#nombre").focusValidations({ 
           reglas: /^[A-Za-z]{10,50}$/,
           mensaje_reglas:"Ingrese solo de 10 a 50 letras"
         });
 
-        $("#email").tonnyvalidations({
+        $("#email").focusValidations({
             mensaje: "No debe quedar vacio el campo email!",
             reglas: /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/,
             mensaje_reglas:"Ingrese un formato de correo electronico!"
         });
 
-        $("#asunto").tonnyvalidations({
+        $("#asunto").focusValidations({
             mensaje: "Se necesita llenar el campo asunto!",
             reglas:/^[e||n||b||s||ins]$/,
             mensaje_reglas:"Ingrese solo e,n,b,s o ins!"
         });
 
-        $("#mensaje").tonnyvalidations({
+        $("#mensaje").focusValidations({
             mensaje: "Se necesita llenar el campo mensaje!"
         });
         return false;
